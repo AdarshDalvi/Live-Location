bb2f90dc306b968a387126a80d4de8a5ed9cb1a8
AdarshDalvi <adarshdalvi197@gmail.com>
2022-09-26 14:10:00 +0530

    Initial commit

---
+++ lib/screens/live_location.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LiveLocation extends StatefulWidget {
+  const LiveLocation({super.key});
+
+  @override
+  State<LiveLocation> createState() => _LiveLocationState();
+}
+
+class _LiveLocationState extends State<LiveLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
